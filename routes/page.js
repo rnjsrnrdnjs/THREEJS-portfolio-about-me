cfee9983e05644af06527451cfbdb4dad743df5d
rnjsrnrdnjs <090tree@gmail.com>
2021-06-19 14:27:29 +0000

    computer

--- routes/page.js
+++ routes/page.js
@@ -1,6 +1,9 @@
 const express=require('express');
-
 const router=express.Router();
+
+const moment=require('moment');
+require('moment-timezone');
+moment.tz.setDefault("Asia/Seoul");
 
 router.use((req,res,next)=>{
 	/* res.locals 값추가 가능*/
@@ -29,21 +32,23 @@ router.get('/food',async(req,res,next)=>{
 });
 router.get('/fairy',async(req,res,next)=>{
 	try{
-		/*
-		const image=[];
-		const aniCount=27
-		for(let i=0;i<aniCount;i++){
-			image.push(`${i}`);
-		}
-		console.log(image);
-		*/
 		res.render('fairy');
 	}catch(err){
 		console.log(err);
 	}
 });
-router.get('/computer',(req,res,next)=>{
-	res.render('computer');
+router.get('/computer',async(req,res,next)=>{
+	try{
+		//시간은 후에 노드 스케쥴러 크론을 이용해 주기적을 보내주자.
+		let now=new Date();
+		let time=moment().format("MM-DD HH:mm");
+		
+		res.render('computer',{
+			time:time,
+		});
+	}catch(err){
+		console.log(err);
+	}
 });
 
 
